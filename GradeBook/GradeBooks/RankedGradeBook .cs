1fc9a82bd3b54403c5ff265c5ac0ad59b76bae2c
Patryk Strzygocki <patryk53@o2.pl>
2022-03-26 01:39:32 +0100

    Override RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook .cs
+++ GradeBook/GradeBooks/RankedGradeBook .cs
@@ -50,5 +50,17 @@ namespace GradeBook.GradeBooks
             }
             throw new InvalidOperationException();
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            } 
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
     }
 }
