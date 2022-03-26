87bbcd79f6d2408a18ec22242d297b43f5414321
Patryk Strzygocki <patryk53@o2.pl>
2022-03-26 02:08:35 +0100

    Add support for weighted GPAs fix

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -91,33 +91,6 @@ namespace GradeBook.UserInterfaces
 
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
-        public static void LoadCommand(string command)
-        {
-            var parts = command.Split(' ');
-            if (parts.Length != 3)
-            {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
-                return;
-            }
-            var type = parts[2];
-            var name = parts[1];
-            BaseGradeBook gradeBook;
-            if (type == "standard")
-            {
-                gradeBook = new StandardGradeBook(name);
-            }
-            else if (type == "ranked")
-            {
-                gradeBook = new RankedGradeBook(name);
-            }
-            else
-            {
-                Console.WriteLine(type + " is not a supported type of gradebook, please try again");
-                return;
-            }
-
-            GradeBookUserInterface.CommandLoop(gradeBook);
-        }
 
         public static void HelpCommand()
         {
